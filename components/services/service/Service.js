a85abba3d90bf58e812f193afe3f71978dd54a29
kozzztia <kozzztia@gmail.com>
2023-02-24 09:58:18 +0200

    refactoring services

--- components/services/service/Service.js
+++ components/services/service/Service.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import style from './style.module.scss';
-import {findIcon} from "../../../utils/findIcon"
+import {findIcon} from "@/utils/findIcon"
 
 
 const Service = ({data}) => {
